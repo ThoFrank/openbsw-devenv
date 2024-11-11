e32b88b39c79d93e59597f55480879cab0b3c6a5
Thomas Frank <thomas.frank@esrlabs.com>
2024-11-11 07:35:46 +0100

    update treefmt to v2.1.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     devenv.url = "github:cachix/devenv";
     mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
-    treefmt.url = "github:numtide/treefmt/v2.0.5";
+    treefmt.url = "github:numtide/treefmt/v2.1.0";
   };
 
   nixConfig = {
